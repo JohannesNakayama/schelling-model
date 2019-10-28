2129ecfc3cea8ec73dcdfebc0868a0d0e02ac2e2
Johannes Nakayama <johannes.nakayama@rwth-aachen.de>
2019-10-28 12:29:24 +0100

    first working version

--- test.jl
+++ test.jl
@@ -1,29 +1,10 @@
-#=
-
-parameters:
-* population density
-* two types of agents
-* tolerance threshold
-
-methods:
-* tick
-* simulate
-* setup
-* check_neighborhood
-
-undecided:
-* agent happiness
-
-if rand() < 0.8 if rand() < 0.5 (first population density then agent type)
-
-=#
-
 # load libraries
 using PyPlot
 
 # setup parameters
 pop_density = 0.80
 dim = 100
+threshold = 0.7
 
 # initialize world
 world = rand(dim, dim)
@@ -39,8 +20,12 @@ for n in 1:dim, m in 1:dim
     end
 end
 
+# show initial state
+PyPlot.imshow(world)
+display(gcf())
+
 # is agent happy?
-function check_agent_happiness(world, i, j, threshold)
+function agent_happy(world, i, j, threshold)
     similar = 0
     different = 0
     positions = [ 
@@ -49,38 +34,84 @@ function check_agent_happiness(world, i, j, threshold)
         (i+1, j-1), (i+1, j  ), (i+1, j+1) 
     ]
     for pos in positions
+        # case 1: row out of bounds (0)
         if pos[1] == 0
+            # case 1a: column out of bounds (0)
             if pos[2] == 0
                 world[size(world)[1], size(world)[1]] == world[i, j] ? similar += 1 : different += 1
+            # case 1b: column out of bounds (dim)
             elseif pos[2] == (size(world)[1] + 1)
                 world[size(world)[1], 1] == world[i, j] ? similar += 1 : different += 1
+            # case 1c: column not out of bounds
             else
                 world[size(world)[1], pos[2]] == world[i, j] ? similar += 1 : different += 1
             end
+        # case 2: row out of bounds (dim)
         elseif pos[1] == (size(world)[1] + 1)
+            # case 2a: column out of bounds (0)
             if pos[2] == 0
                 world[1, size(world)[1]] == world[i, j] ? similar += 1 : different += 1
+            # case 2b: column oht of bounds (dim)
             elseif pos[2] == (size(world)[1] + 1)
                 world[1, 1] == world[i, j] ? similar += 1 : different += 1
+            # case 2c: column not out of bounds
             else
                 world[1, pos[2]] == world[i, j] ? similar += 1 : different += 1
             end
+        # case 3: row not out of bounds
         else
+            # case 3a: column out of bounds (0)
             if pos[2] == 0
                 world[pos[1], size(world)[1]] == world[i, j] ? similar += 1 : different += 1
+            # case 3b: column out of bounds (dim)
             elseif pos[2] == (size(world)[1] + 1)
                 world[pos[1], 1] == world[i, j] ? similar += 1 : different += 1
+            # case 3c: column not out of bounds
             else
                 world[pos[1], pos[2]] == world[i, j] ? similar += 1 : different += 1
             end        
         end
     end
+    # return true if relative count of similar agents in neighborhood is > threshold, else false
     return similar/different >= threshold ? true : false
-    # return similar, different
 end
 
+# if agent is unhappy: relocate
 function relocate(world, i, j)
+    new_row = convert(Int8, ceil(rand() * 100))
+    new_col = convert(Int8, ceil(rand() * 100))
+    if world[new_row, new_col] == 0.0
+        world[new_row, new_col] = world[i, j]
+        world[i, j] = 0.0
+    else
+        relocate(world, i, j)
+    end
+    return world
 end
 
-PyPlot.imshow(world)
+# simulation step
+function tick(world)
+    for n in 1:dim, m in 1:dim
+        if !agent_happy(world, n, m, threshold)
+            relocate(world, n, m)
+        end
+    end
+    return world
+end
+
+# run the simulation for 100 steps
+function simulate(world)
+    for step in 1:1000
+        world = tick(world)
+    end
+    return world
+end
+
+# execute simulation
+new_world = simulate(world)
+
+# show state after simulation
+PyPlot.clf()
+PyPlot.imshow(new_world)
 display(gcf())
+
